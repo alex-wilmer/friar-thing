45f19b89fd4475cd07a9af6eeb68c25bcfb37184
Alex Wilmer <djazium@gmail.com>
2018-06-11 17:14:32 -0400

    my first commit!

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to the friar@@@@!!!!</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <button>click to see the menu</button>
+      </div>
+    );
+  }
+}
+
+export default App;
